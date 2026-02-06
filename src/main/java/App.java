37618f4d4458cfb4951c285af48385892a6bcea6
5s-mini <dnclsehd123@naver.com>
2026-02-06 12:24:17 +0900

    feat: 등록 기능 구현

---
+++ src/main/java/App.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class App {
+
+    private Scanner sc;
+
+    public App(Scanner sc) {
+        this.sc = sc;
+    }
+
+    public void run() {
+        System.out.println("== 명언 앱 ==");
+
+        while (true) {
+            System.out.print("명령) ");
+            String cmd = sc.nextLine();
+
+            switch (cmd) {
+                case "등록" -> {
+                    System.out.print("명언 : ");
+                    String saying = sc.nextLine();
+                    System.out.print("작가 : ");
+                    String author = sc.nextLine();
+
+                    System.out.println("1번 명언이 등록되었습니다.");
+                }
+                case "종료" -> {
+                    return;
+                }
+            }
+        }
+    }
+}
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,7 +1,5 @@
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.io.ByteArrayOutputStream;
-import java.util.Scanner;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,14 +7,29 @@ public class AppTest {
 
     @Test
     @DisplayName("'== 명언 앱 ==' 출력")
-    void t1() {
-        Scanner sc = TestUtil.genScanner("종료");
-
-        ByteArrayOutputStream outputStream = TestUtil.setOutByteArray();
-        new App(sc).run();
-
-        String out = outputStream.toString();
+    void t1() throws Exception {
+        String out = AppTestRunner.run ("""
+                종료
+                """
+        );
 
         assertThat(out).contains("== 명언 앱 ==");
+    }
+
+    @Test
+    @DisplayName("등록")
+    void t2() {
+        String out = AppTestRunner.run ("""
+                등록
+                현재를 사랑하라
+                작자미상
+                종료
+                """
+        );
+
+        assertThat(out).contains("명령) ");
+        assertThat(out).contains("명언: ");
+        assertThat(out).contains("작가: ");
+        assertThat(out).contains("1번 명언이 등록되었습니다.");
     }
 }
